fdff3f68cfd75d8e6a16a3a8bb702c3f37c13e20
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-04-30 17:02:42 -0400

    More copy pasta from reading.

--- quiz-app.py
+++ quiz-app.py
@@ -4,8 +4,14 @@
 
 import sqlite3
 from flask import Flask, render_template, request, redirect
+from contextlib import closing
 app = Flask(__name__)
 
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 def connect_db():
     return sqlite3.connect(app.config['hw13.db'])
