64235b2761992af646d85c6c590c25479c0740e6
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-05-01 13:13:55 -0400

    More copy pasta from reading.

--- quiz-app.py
+++ quiz-app.py
@@ -36,12 +36,12 @@ def show_entries():
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
-@app.route('/add', methods=['POST'])
+@app.route('/student/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    g.db.execute('insert into Students (firstname, lastname) values (?, ?)',
+                 [request.form['firstname'], request.form['lastname']])
     g.db.commit()
-    flash('New entry was successfully posted')
+    flash('New student record was successfully added.')
     return redirect(url_for('show_entries'))
